293bfd077cb62059979ff7e807a01fe8145c76bf
ElaineCosta <elainerscosta@hotmail.com>
2022-08-15 17:00:13 -0300

    feat: criando funcao create no Controller para responder ao cliente sobre a inserção da informação positiva  no banco de dados

--- controllers/productsControllers.js
+++ controllers/productsControllers.js
@@ -12,6 +12,18 @@ const productsControllers = {
     if (!productId) return res.status(404).json({ message: 'Product not found' });
     return res.status(200).json(productId);
   },
+
+  create: async (req, res) => {
+    const { name } = req.body;
+    const productResult = await productsServices.create(name);
+
+    // if (productResult.affectedRows === 0) return ;
+  
+    return res.status(201).json({
+      id: productResult.insertId,
+      name,
+    });
+  },
 };
 
 module.exports = productsControllers;
