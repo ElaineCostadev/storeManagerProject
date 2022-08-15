7989c70e9c2d333a1269edf29208a6f5a2566b4e
ElaineCosta <elainerscosta@hotmail.com>
2022-08-15 17:00:49 -0300

    feat: criando funcao post na rota para nova funcionalidade de inserir informações do produto no banco de dados

--- routes/productsRoute.js
+++ routes/productsRoute.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const productsControllers = require('../controllers/productsControllers');
+const validateProducts = require('../middlewares/validateProducts');
 
 const productRoute = express.Router();
 
 productRoute.get('/', productsControllers.getAll);
 productRoute.get('/:id', productsControllers.getByPk);
+
+productRoute.post('/', validateProducts, productsControllers.create);
 
 module.exports = productRoute;
