a65de39eeb57f276ecddb95f31e550a6a0dba59d
ElaineCosta <elainerscosta@hotmail.com>
2022-08-15 16:57:53 -0300

    feat: criando funcao de middleware para validação dos produtos inseridos pelo cliente

---
+++ middlewares/validateProducts.js
@@ -0,0 +1,14 @@
+const CustomError = require('../errors/CustomError');
+
+const validateProducts = async (req, res, next) => {
+  const { name } = req.body;
+  if (!name) throw new CustomError(400, '"name" is required');
+    // res.status(400).json({ message: '"name" is required' });
+  if (name.length < 5) {
+    throw new CustomError(422, '"name" length must be at least 5 characters long');
+    // res.status(422).json({ message: '"name" length must be at least 5 characters long' });
+}
+  next();
+};
+
+module.exports = validateProducts;
