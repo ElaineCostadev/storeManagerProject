d827c4a1f7156ed23aeef66b9cdb6ab0631e02fe
ElaineCosta <elainerscosta@hotmail.com>
2022-08-17 19:28:32 -0300

    fix: ajustando a logica da funcao createSales do controller

--- controllers/salesControllers.js
+++ controllers/salesControllers.js
@@ -2,28 +2,22 @@ const salesService = require('../services/salesServices');
 
 const salesController = {
   createSales: async (req, res) => {
-    const { idSale, idProcuct, quantity, date } = req.body;
-    const sales = await salesService.createSales(idSale, idProcuct, quantity, date);
-
-    return res.status(201).json({
-      id: sales.idSale.insertId,
-      itemsSold: [{
-        productId: sales.idProcuct.insertId,
-        quantity: sales.quantity,
-      }],
-    });
+    const sale = req.body;
+    const sales = await salesService.createSales(sale);
+    // if (sales.message) return res.status(404).json({ message: 'Product not found' });
+    return res.status(201).json(sales);
   },
-
-    getAll: async (_req, res) => {
-      const salesResponse = await salesService.getAll();
-      res.status(200).json(salesResponse);
+  
+  getAll: async (_req, res) => {
+    const salesResponse = await salesService.getAll();
+    res.status(200).json(salesResponse);
   },
     
   getByPk: async (req, res) => {
     const { id } = req.params;
-    const salesResponse = await salesService.getByPk(id);
-    res.status(200).json(salesResponse);
-    },
+      const salesResponse = await salesService.getByPk(id);
+      res.status(200).json(salesResponse);
+  },
   
 };
 
