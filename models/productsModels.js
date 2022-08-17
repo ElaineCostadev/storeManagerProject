20d74f99be8a32fd4ba4a32bf5a24b35252f9d36
ElaineCosta <elainerscosta@hotmail.com>
2022-08-17 19:22:49 -0300

    acrescentando funcoes de update e exclude na camada de model dos products

--- models/productsModels.js
+++ models/productsModels.js
@@ -5,16 +5,29 @@ const productsModel = {
     const [products] = await connection.execute('SELECT * FROM StoreManager.products;');
     return products;
   },
-
+  
   getByPk: async (id) => {
     const [[products]] = await connection.execute(`SELECT * FROM StoreManager.products
-    WHERE id=? ORDER BY id;`, [id]);
+    WHERE id = ? ORDER BY id;`, [id]);
+    console.log(products);
     return products;
   },
 
   create: async (nameProduct) => {
     const [product] = await connection
-      .execute('INSERT INTO StoreManager.products(name) VALUES (?);', [nameProduct]);
+      .execute('INSERT INTO StoreManager.products (name) VALUES (?);', [nameProduct]);
+    return product;
+  },
+
+  update: async (id, name) => {
+    const [products] = await connection
+      .query('UPDATE StoreManager.products SET name = ? WHERE id = ?;', [name, id]);
+    return products;
+  },
+
+  exclude: async (id) => {
+    const [product] = await connection
+      .execute('DELETE FROM StoreManager.products WHERE id = ?;', [id]);
     return product;
   },
   
