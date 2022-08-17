c19b8c61e1895d5b8c691c00996da436bded7ded
ElaineCosta <elainerscosta@hotmail.com>
2022-08-17 19:29:45 -0300

    fix: ajustando a logica da funcao createSales do Services  e dos tratamentos de erros

--- services/salesServices.js
+++ services/salesServices.js
@@ -1,18 +1,32 @@
 const CustomError = require('../errors/CustomError');
+const productsModel = require('../models/productsModels');
 const salesModel = require('../models/salesModels');
 
+// https://stackoverflow.com/questions/37576685/using-async-await-with-a-foreach-loop
+// e course para lembrar do promisse all
 const salesService = {
-  createSales: async (idSale, idProcuct, quantity, date) => {
-    const dateSale = await salesModel.createDate(date);
-    const sales = await salesModel.createSales(idSale, idProcuct, quantity);
-    return { sales, dateSale };
+  createSales: async (sales) => {
+    const allProducts = await productsModel.getAll();
+
+    const checkIfProductExists = sales.every((eachItens) => allProducts
+      .some((eachProduct) => eachProduct.id === eachItens.productId));
+    
+    if (!checkIfProductExists) throw new CustomError(404, 'Product not found');
+
+/*      await Promise.all(sales.forEach(async (eachProductMap) => {
+       const verify = await productsModel.getByPk(eachProductMap.productId);
+      if (verify === undefined) throw new CustomError(404, 'Product not found');
+    }));
+ */
+    const salesProducts = await salesModel.createSales(sales);
+    return salesProducts;
   },
 
   getAll: async () => {
     const salesResult = await salesModel.getAll();
     const mapSales = salesResult.map((eachSale) => {
       const resultObj = {
-        saleId: eachSale.sale_id,
+        saleId: eachSale.id,
         date: eachSale.date,
         productId: eachSale.product_id,
         quantity: eachSale.quantity,
@@ -22,12 +36,11 @@ const salesService = {
     return mapSales;
   },
 
-  getByPk: async (id) => {
+  getByPk: async (id) => { 
+    const verifySale = await salesModel.checkIfSalesExists(id);
+    if (verifySale.length === 0) throw new CustomError(404, 'Sale not found');
+
     const salesResult = await salesModel.getByPk(id);
-
-    const findId = salesResult.find((eachSale) => eachSale.id !== +id);
-    if (!findId) throw new CustomError(404, 'Sale not found');
-
     const mapSales = salesResult.map((eachSale) => {
       const resultObj = {
         date: eachSale.date,
