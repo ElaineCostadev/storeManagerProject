904a3ea9fa697710db41643d6db2152a661929c6
ElaineCosta <elainerscosta@hotmail.com>
2022-08-17 19:25:11 -0300

    fix: ajustando a logica de validação de Sales

--- middlewares/validateSales.js
+++ middlewares/validateSales.js
@@ -1,22 +1,20 @@
 const CustomError = require('../errors/CustomError');
 
-// da tabela sales é apenas a data
-// da tabela sales_product é o sale_id, product_id, quantity
-// preciso transformar snac_case em camelCase - date, sale_id, product_id, quantity
+const validateSales = (req, res, next) => {
+  const { body } = req;
 
-const validadeSales = (req, res, next) => {
-  const { /* date  saleId, */ productId, quantity } = req.body;
-  // se não cadastrar essa informação
-  if (!productId) throw new CustomError(400, '"productId" is required');
+  const verifyProduct = body.some((eachProduct) => eachProduct.productId === undefined);
+  const verifyQuantity = body.some((eachProduct) => eachProduct.quantity === undefined);
+  const verifyQuantityNumber = body.some((eachProduct) => eachProduct.quantity <= 0);
 
-  // se não existir esse produto no meu banco de dados
-  if (productId === undefined) throw new CustomError(404, 'Product not found');
+  if (verifyProduct) throw new CustomError(400, '"productId" is required');
 
-  if (!quantity) throw new CustomError(400, '"quantity" is required');
-
-  if (quantity <= 1) throw new CustomError(422, '"quantity" must be greater than or equal to 1');
-
+  if (verifyQuantityNumber) {
+    throw new CustomError(422, '"quantity" must be greater than or equal to 1');
+  }
+  if (verifyQuantity) throw new CustomError(400, '"quantity" is required');
+  
   next();
 };
 
-module.exports = { validadeSales };
+module.exports = validateSales;
