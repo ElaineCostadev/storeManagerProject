d471dbab817c63329fa57ffcb5df7d898b76ec18
ElaineCosta <elainerscosta@hotmail.com>
2022-08-17 19:29:11 -0300

    fix: ajustando a logica da funcao createSales do Models e das Querys

--- models/salesModels.js
+++ models/salesModels.js
@@ -1,39 +1,59 @@
 const connection = require('./connection');
 
 const salesModel = {
-  createDate: async (date) => {
-    const [sales] = await connection
-      .execute('INSERT INTO StoreManager.sales (date) VALUES (?);', [date]);
-    return sales;
-  },
 
-  createSales: async (idSale, idProcuct, quantity) => {
-    const [salesProducts] = await connection
-      .execute(`INSERT INTO StoreManager.sales_products (sale_id, product_id, quantity)
-      VALUES (?, ?, ?);`, [{ idSale, idProcuct, quantity }]);
-    return salesProducts;
+  createSales: async (sales) => {
+    const [salesId] = await connection.execute(`INSERT INTO StoreManager.sales (date)
+      VALUES (NOW());`);
+    // aqui encontro o id da tabela sale - o sale_id
+
+    await sales.forEach(async (eachProduct) => {
+      await connection
+        .execute(`INSERT INTO StoreManager.sales_products (sale_id, product_id, quantity)
+          VALUES (?, ?, ?);`, [salesId.insertId, eachProduct.productId, eachProduct.quantity]);
+      // aqui insiro o id de sale_id - tbm o Id de cada produto e a quantidade de cada produto
+    });
+
+    // retorno o objeto com o id de saleID e a chave dos items com o array de produtos recebido no parametro
+
+    return { id: salesId.insertId, itemsSold: sales };
   },
 
   getAll: async () => {
-    const [sales] = await connection.execute(`SELECT sale_id, date, product_id, quantity
-      FROM StoreManager.sales_products
-      JOIN StoreManager.products
-      ON StoreManager.sales_products.product_id = StoreManager.products.id
-      JOIN StoreManager.sales
-      ON StoreManager.sales_products.sale_id = StoreManager.sales.id;`);
+    const [sales] = await connection.execute(`
+      SELECT
+        s.id, s.date, sp.product_id, sp.quantity
+      FROM
+        StoreManager.sales AS s
+      INNER JOIN
+        StoreManager.sales_products AS sp
+      ON
+        s.id = sp.sale_id;`);
     return sales;
   },
 
   getByPk: async (id) => {
-    const [sales] = await connection.execute(`SELECT date, product_id, quantity
-      FROM StoreManager.sales_products
-      JOIN StoreManager.products
-      ON StoreManager.sales_products.product_id = StoreManager.products.id
-      JOIN StoreManager.sales
-      ON StoreManager.sales_products.sale_id = StoreManager.sales.id
-      WHERE sale_id = ?;`, [id]);
+    const [sales] = await connection.execute(`
+      SELECT
+        s.date, sp.product_id, sp.quantity
+      FROM
+          StoreManager.sales AS s
+      INNER JOIN
+        StoreManager.sales_products AS sp
+      ON
+        s.id = sp.sale_id
+      WHERE
+        s.id= ?
+      ORDER BY
+        s.id ASC, sp.product_id ASC;`, [id]);
 
       return sales;
+  },
+
+  checkIfSalesExists: async (id) => {
+      const [sale] = await connection.execute(`SELECT * FROM StoreManager.sales
+    WHERE id = ?`, [id]);
+      return sale;
   },
 };
 
