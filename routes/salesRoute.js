c34819fc08a9cb53efa10e7a0d50e8647b0ee48a
ElaineCosta <elainerscosta@hotmail.com>
2022-08-17 19:25:39 -0300

    fix: ajustando a routea de validação de Sales

--- routes/salesRoute.js
+++ routes/salesRoute.js
@@ -3,11 +3,12 @@ const express = require('express');
 const salesRoute = express.Router();
 
 const salesController = require('../controllers/salesControllers');
-// const validate = require('../middlewares/validateSales');
+const validateSales = require('../middlewares/validateSales');
+// const validateProducts = require('../middlewares/validateProducts');
 
 salesRoute.get('/', salesController.getAll);
 salesRoute.get('/:id', salesController.getByPk);
 
-salesRoute.post('/', /* validateSales */ salesController.createSales);
+salesRoute.post('/', validateSales, salesController.createSales);
 
 module.exports = salesRoute;
