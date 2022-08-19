f74538e21402dfcab339e3c7311a69c85172a813
ElaineCosta <elainerscosta@hotmail.com>
2022-08-18 21:19:47 -0300

    feat e fix: incluindo e ajustando os testes da camada productService

--- tests/unit/services/productsServices.test.js
+++ tests/unit/services/productsServices.test.js
@@ -11,23 +11,34 @@ chai.use(chaiAsPromised);
 
 
 describe('Verificando testes do productsServices', () => {
-  describe('Verifica quando é chamado endpoint get. no /products', () => {
+  describe('Verifica quando é chamado a funcao getAll no /products', () => {
 
+    const returnResultExpect = [
+      { id: 1, name: 'Martelo de Thor' },
+      { id: 2, name: 'Traje de encolhimento' },
+      { id: 3, name: 'Escudo do Capitão América' },
+    ]
     afterEach(() => Sinon.restore())
 
-    it('Quando é chamado com sucesso e aparecem todos os produtos no getAll', async () => {
-      const returnResultExpect = [
-        { id: 1, name: 'Martelo de Thor' },
-        { id: 2, name: 'Traje de encolhimento' },
-        { id: 3, name: 'Escudo do Capitão América' },
-      ]
+    it('Quando é chamado com sucesso e aparecem todos os produtos em um array', async () => {
       Sinon.stub(productsModel, 'getAll').resolves([returnResultExpect]);
 
       const [resultService] = await productsService.getAll();
 
       expect(resultService).to.be.deep.equal(returnResultExpect);
-
     });
+
+    it('Quando é chamado com sucesso e aparecem todos os produtos com as chaves obrigatorias', async () => {
+      Sinon.stub(productsModel, 'getAll').resolves(returnResultExpect);
+
+      const [resultModel] = await productsService.getAll();
+
+      expect(resultModel).to.all.keys('id', 'name');
+    });
+  });
+
+  describe('Verifica quando é chamado a funcao getbyPk no /products', () => {
+      afterEach(() => Sinon.restore())
 
     it('Quando é chamado com sucesso com ID e aparece apenas 1 product no getByPk', async () => {
       const returnResultExpect = { id: 1, name: 'Martelo de Thor' }
@@ -36,10 +47,11 @@ describe('Verificando testes do productsServices', () => {
       const resultService = await productsService.getByPk(1);
 
       expect(resultService).to.be.equal(returnResultExpect);
+      expect(resultService).to.be.an('object')
 
     });
-
-    it('Quando é chamado e o ID não existir no getByPk ', async () => {
+    
+    it('Quando é chamado a função getByPk e não existe o ID do produto', async () => {
       const returnResultExpect = {
         "message": "Product not found"
       }
@@ -50,44 +62,87 @@ describe('Verificando testes do productsServices', () => {
 
       expect(resultService).to.have.key('message');
       expect(resultService).to.be.a('object');
-
     });
+
+    it('Quando é chamado a função getByPk, o CustomError lança o erro com a message onde não existe o ID do produto', async () => {
+
+      return expect(productsService.getByPk(4))
+        .to.eventually.be.rejectedWith('Product not found')
+        .and.be.an.instanceOf(CustomError)
+        .and.have.property('status', 404);
+    });
+
+  });
+
+  describe('Verifica quando é chamado a funcao create no /products', () => {
+  
+    afterEach(() => Sinon.restore())
 
     it('Quando o produto é cadastrado com sucesso', async () => {
-      const returnResultExpect = { name: 'ProductOk' }
+      const returnResultExpect = { id: 4, name: 'Jaqueline' }
 
       Sinon.stub(productsModel, 'create').resolves(returnResultExpect);
-      const resultService = await productsService.create();
+      const resultService = await productsService.create('Jaqueline');
 
       expect(resultService).to.be.a('object');
-      expect(resultService).to.have.key('name');
+      expect(resultService).to.have.keys('id', 'name');
+    });
+  });
+    
+  describe('Verifica quando é chamado a funcao Update no /products', () => {
+
+    afterEach(() => Sinon.restore())
+
+    it('Quando o produto é atualizado com sucesso', async () => {
+      const returnResultExpect = { id: 1, name: 'Martelo de Thor' }
+      Sinon.stub(productsModel, 'getByPk').resolves(returnResultExpect);
+
+      await productsService.getByPk(1);
+
+      Sinon.stub(productsModel, 'update').resolves(returnResultExpect);
+
+      const resultService = await productsService.update(returnResultExpect);
+
+      expect(resultService).to.be.a('object');
+      expect(resultService).to.have.keys( 'id', 'name');
     });
 
-/*     describe('Validações do Middleware de erro', () => {
-      it('Quando não é possivel cadastrar o produto pelo nome', async () => {
-        const req = {};
-        const res = {};
+    it('Quando o produto não é atualizado e é lançado um erro com CustomError', async () => {
+ 
+      const returnResultExpect = { id: 4, name: 'Jaqueline' }
 
-        res.status = Sinon.stub().returns(res);
-        res.json = Sinon.stub().returns();
-        req.body = {};
-        
-        
+      Sinon.stub(productsModel, 'getByPk').resolves(returnResultExpect);
 
-        return expect(await validateProducts(req, res))
-          .to.be.rejectedWith('name is required' )
-          .and.be.an.instanceOf(CustomError)
-          .and.to.include({ status: 400 })
-        
-        
-       //  expect(res.status.calledWith(404)).to.be.equal(false)
-        // res.status(400).json({ message: '"name" is required' });
-        
-      });
-
-
-
-    }); */
+      return expect(productsService.update({ id: 9, name: 'NovoUpdate' }))
+        .to.eventually.be.rejectedWith('Product not')
+        .and.be.an.instanceOf(CustomError)
+        .and.have.property('status', 404);
+      
+      // a minha frase está com falso positivo?
+    });
   });
+
+  describe('Verifica quando é chamado a funcao Exclude no /products', () => {
+
+    afterEach(() => Sinon.restore())
+
+    it('Quando o produto é excluido com sucesso', async () => {
+      const returnResultExpect = { id: 2 }
+
+      Sinon.stub(productsModel, 'exclude').resolves(returnResultExpect);
+      const resultService = await productsService.exclude(2);
+
+      expect(resultService).to.be.an('object');
+    });
+
+    it('Quando o produto não é encontrado para ser excluido', async () => {
+      return expect(productsService.exclude(5))
+        .to.eventually.be.rejectedWith('Product not found')
+        .and.be.an.instanceOf(CustomError)
+        .and.have.property('status', 404);
+    });
+
+  });
+
 });
 
