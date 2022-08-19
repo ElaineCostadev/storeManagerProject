78ffd09b9970d5c946b2a133205fa96cb5e3c914
ElaineCosta <elainerscosta@hotmail.com>
2022-08-18 21:15:30 -0300

    fix: ajustando camada de products Controller

--- controllers/productsControllers.js
+++ controllers/productsControllers.js
@@ -3,39 +3,37 @@ const productsServices = require('../services/productsServices');
 const productsControllers = {
   getAll: async (req, res) => {
     const productsResponse = await productsServices.getAll();
-    res.status(200).json(productsResponse);
+
+    return res.status(200).json(productsResponse);
   },
 
   getByPk: async (req, res) => {
     const { id } = req.params;
     const productId = await productsServices.getByPk(id);
-    // if (!productId) return res.status(404).json({ message: 'Product not found' });
+
     return res.status(200).json(productId);
   },
 
   create: async (req, res) => {
     const { name } = req.body;
     const productResult = await productsServices.create(name);
-
-    // if (productResult.affectedRows === 0) return ;
-  
-    return res.status(201).json({
-      id: productResult.insertId,
-      name,
-    });
+ 
+    return res.status(201).json({ id: productResult.insertId, name });
   },
 
   update: async (req, res) => {
     const { id } = req.params;
     const { name } = req.body;
     const productId = await productsServices.update({ id, name });
+
     return res.status(200).json(productId);
   },
 
   exclude: async (req, res) => {
     const { id } = req.params;
     await productsServices.exclude(id);
-    return res.status(204).end('deu certo');
+
+    return res.status(204).end();
   },
 };
 
