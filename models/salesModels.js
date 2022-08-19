d864fa831028d0a6bdd2b92fa6dfaac35414b763
ElaineCosta <elainerscosta@hotmail.com>
2022-08-18 21:17:48 -0300

    fix: ajustando camada de sales Model  - reordenando as funcoes, reescrevendo querys e retirando os logs

--- models/salesModels.js
+++ models/salesModels.js
@@ -1,6 +1,36 @@
 const connection = require('./connection');
 
 const salesModel = {
+  getAll: async () => {
+    const [sales] = await connection.execute(`
+      SELECT
+        s.id AS saleId, s.date, sp.product_id AS productId, sp.quantity
+      FROM
+        StoreManager.sales AS s
+      INNER JOIN
+        StoreManager.sales_products AS sp
+      ON
+        s.id = sp.sale_id;`);
+    return sales;
+  },
+  
+  getByPk: async (id) => {
+    const [sales] = await connection.execute(`
+      SELECT
+        s.date , sp.product_id AS productId, sp.quantity
+      FROM
+          StoreManager.sales AS s
+      INNER JOIN
+        StoreManager.sales_products AS sp
+      ON
+        s.id = sp.sale_id
+      WHERE
+        s.id= ?
+      ORDER BY
+        s.id ASC, sp.product_id ASC;`, [id]);
+
+      return sales;
+  },
 
   createSales: async (sales) => {
     const [salesId] = await connection.execute(`INSERT INTO StoreManager.sales (date)
@@ -19,41 +49,26 @@ const salesModel = {
     return { id: salesId.insertId, itemsSold: sales };
   },
 
-  getAll: async () => {
-    const [sales] = await connection.execute(`
-      SELECT
-        s.id, s.date, sp.product_id, sp.quantity
-      FROM
-        StoreManager.sales AS s
-      INNER JOIN
-        StoreManager.sales_products AS sp
-      ON
-        s.id = sp.sale_id;`);
-    return sales;
-  },
-
-  getByPk: async (id) => {
-    const [sales] = await connection.execute(`
-      SELECT
-        s.date, sp.product_id, sp.quantity
-      FROM
-          StoreManager.sales AS s
-      INNER JOIN
-        StoreManager.sales_products AS sp
-      ON
-        s.id = sp.sale_id
-      WHERE
-        s.id= ?
-      ORDER BY
-        s.id ASC, sp.product_id ASC;`, [id]);
-
-      return sales;
-  },
-
   checkIfSalesExists: async (id) => {
-      const [sale] = await connection.execute(`SELECT * FROM StoreManager.sales
-    WHERE id = ?`, [id]);
-      return sale;
+    const [sale] = await connection.execute('SELECT * FROM StoreManager.sales WHERE id = ?', [id]);
+    return sale;
+  },
+
+  update: async (id, body) => {
+    await body.forEach(async (eachProduct) => {
+      await connection
+        .execute(`UPDATE StoreManager.sales_products
+                  SET product_id = ?, quantity = ?
+                  WHERE sale_id = ?;`,
+          [eachProduct.productId, eachProduct.quantity, id]);
+    });
+    return { saleId: id, itemsUpdated: body };
+  },
+
+  exclude: async (id) => {
+    const [saleExcluide] = await connection
+      .execute('DELETE FROM StoreManager.sales WHERE id = ?;', [id]);
+    return saleExcluide;
   },
 };
 
